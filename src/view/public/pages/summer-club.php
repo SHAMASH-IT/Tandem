3c965b23ee31f6490dcc418cc6f85ca8391c0d55
Younes Nidhal <nidhal.benyounes@yahoo.fr>
2024-06-10 17:40:08 +0100

    Google-Tag-Manager-v5

--- src/view/public/pages/summer-club.php
+++ src/view/public/pages/summer-club.php
@@ -11,41 +11,7 @@
 
     require_once 'src/view/public/includes/header.php';
 
-    $head_content = <<<HTML
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'
-https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TRTMT36Z');</script>
-<!-- End Google Tag Manager -->
-HTML;
-
-// Your PHP code continues here
-
-// Placeholder for body content
-$body_content = <<<HTML
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRTMT36Z"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-HTML;
-
-// Your PHP code continues here
-
-// Output the HTML with placeholders
-echo <<<HTML
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    $head_content
-</head>
-<body>
-    $body_content
-</body>
-</html>
-HTML;
+    
 
 ?>
 
