2b144673b67765f42f10764f543f7047ef8e1621
Younes Nidhal <nidhal.benyounes@yahoo.fr>
2024-06-10 12:16:25 +0100

    Google-Tag-Manager-v1

--- src/view/public/pages/summer-club.php
+++ src/view/public/pages/summer-club.php
@@ -10,6 +10,17 @@
     $meta_facebook_height = "1600";
 
     require_once 'src/view/public/includes/header.php';
+
+    echo '<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+    new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+    \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,\'script\',\'dataLayer\',\'GTM-TRTMT36Z\');</script>
+    <!-- End Google Tag Manager -->';
+    echo '<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TRTMT36Z" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->';
 ?>
 
     <style>
